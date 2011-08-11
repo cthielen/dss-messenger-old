083a47cc4e3ab00eb95d7199fe40cfa3ab62d65a
Christopher Thielen <cthielen@gmail.com>
2011-08-11 16:12:31 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DssMessenger::Application.config.secret_token = '7d62ece7adb462ca36ff25fbcbac4fb44fa8de7ba0a9cf491fc2faae37a8a994ced2c9a35d1b0f1f5a152ba839934d511e8c5c5264c389f1578234fbed00398f'

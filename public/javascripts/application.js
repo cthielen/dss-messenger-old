ad65536722fa08c454be2d5793378dd2c73e58fc
Christopher Thielen <cthielen@gmail.com>
2011-08-25 16:53:57 -0700

    Additional HTML/CSS/JS fixes.

---
+++ public/javascripts/application.js
@@ -0,0 +1,9 @@
+base_uri = "";
+
+$(function() {
+  $("#to_tokens").tokenInput(base_uri + "/people.json", {
+    crossDomain: false,
+    prePopulate: $("#group_people_tokens").data("pre"),
+    theme: "facebook"
+  });
+});

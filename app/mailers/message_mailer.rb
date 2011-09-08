3c7b3273fb19d6bdc26c238141a34fef0455ca90
Christopher Thielen <cthielen@gmail.com>
2011-09-07 18:00:05 -0700

    Fixed MIME issues with e-mail.

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,10 +2,13 @@ class MessageMailer < ActionMailer::Base
   default :from => "ithelp@dss.ucdavis.edu"
   
   def regular_message(message)
-    bcc message.recipient_emails
-    recipients "Undisclosed Recipients <nobody@ucdavis.edu>"
-    from message.sender.email
-    subject message.subject
-    body :message => message
+    @message = message
+    h = {
+      :recipients => "Undisclosed Recipients <nobody@ucdavis.edu>",
+      :from     => message.sender.email,
+      :subject  => message.subject,
+      :bcc      => message.recipient_emails
+    }
+    mail(h)
   end
 end

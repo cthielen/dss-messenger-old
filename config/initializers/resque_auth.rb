66bc49cc171530d99ef113a585ca36081137a8c1
Christopher Thielen <cthielen@gmail.com>
2011-09-08 15:50:50 -0700

    Updated API key example and added entry for Resque credentials.

--- config/initializers/resque_auth.rb
+++ config/initializers/resque_auth.rb
@@ -1,3 +1,6 @@
 Resque::Server.use(Rack::Auth::Basic) do |user, password|
-  password == "secret"
+  name = YAML.load_file("#{Rails.root.to_s}/config/api_key.yml")['resque']['name']
+  pw = YAML.load_file("#{Rails.root.to_s}/config/api_key.yml")['resque']['password']
+  
+  (user == name) && (password == pw)
 end

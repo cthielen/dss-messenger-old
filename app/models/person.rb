4b329e4dae2db78e5400b4b989b83378860d7217
Christopher Thielen <cthielen@gmail.com>
2011-09-14 14:57:24 -0700

    Removed rvmrc from project

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveResource::Base
   self.site = YAML.load_file("#{Rails.root.to_s}/config/api_keys.yml")['keys']['uri']
   self.user = YAML.load_file("#{Rails.root.to_s}/config/api_keys.yml")['keys']['name']
   self.password = YAML.load_file("#{Rails.root.to_s}/config/api_keys.yml")['keys']['key']
-  self.format = :xml 
+  self.format = :xml
   
   # ACL symbols
   def role_symbols

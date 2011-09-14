f31fcc241e9a11a419283f1b3e287bdb860fb980
Christopher Thielen <cthielen@gmail.com>
2011-09-14 15:33:43 -0700

    Asset path corrections for Rails 3.1

--- app/assets/javascripts/common/facebox.js
+++ app/assets/javascripts/common/facebox.js
@@ -79,8 +79,8 @@
     settings: {
       opacity      : 0,
       overlay      : true,
-      loadingImage : '/assets/loading.gif',
-      closeImage   : '/assets/closelabel.gif',
+      loadingImage : '../../images/loading.gif',
+      closeImage   : '../../images/closelabel.gif',
       imageTypes   : [ 'png', 'jpg', 'jpeg', 'gif' ],
       faceboxHtml  : '\
     <div id="facebox" style="display:none;"> \
@@ -97,7 +97,7 @@
                 </div> \
                 <div class="footer"> \
                   <a href="#" class="close"> \
-                    <img src="/assets/closelabel.gif" title="close" class="close_image" /> \
+                    <img src="../../images/closelabel.gif" title="close" class="close_image" /> \
                   </a> \
                 </div> \
               </td> \
--- config/deploy.rb
+++ config/deploy.rb
@@ -18,24 +18,24 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 namespace :deploy do
+  desc "Pre-compile the assets."
+  task :precompile_assets do
+    run "cd #{current_path}; rm -rf public/assets/*"
+    run "cd #{current_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+  end
+
   desc "Tell Passenger to restart the app."
-    task :restart do
-      run "touch #{current_path}/tmp/restart.txt"
-    end
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
 
-    desc "Symlink shared configs and folders on each release."
-    task :symlink_shared do
-      run "mkdir -p #{shared_path}/config"
-      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-      run "ln -nfs #{shared_path}/config/api_keys.yml #{release_path}/config/api_keys.yml"
-      run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
-    end
-
-    desc "Pre-compile the assets."
-    task :precompile_assets do #, :roles => :web, :except => { :no_release => true } do
-      run "cd #{current_path}; rm -rf public/assets/*"
-      run "cd #{current_path}; RAILS_ENV=production bundle exec rake assets:precompile"
-    end
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "mkdir -p #{shared_path}/config"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/api_keys.yml #{release_path}/config/api_keys.yml"
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+  end
 end
 
 after 'deploy:update_code', 'deploy:symlink_shared'

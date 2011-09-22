3df891fa77f0594ea0b6a0ac1ce102cf9c63f351
Christopher Thielen <cthielen@gmail.com>
2011-09-22 12:01:07 -0700

    Include group and OU-based messages in the API.

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,17 +1,26 @@
 class Api::MessagesController < Api::BaseController
   # GET /api/messages/person_id
   def show
-    @user_id = Person.find(params[:id])
+    user = Person.find(params[:id])
+
+    @messages = []
 
     # Magic numbers here: 1 and 2 are the message types involving "portal inbox" type messages
     # Look up messages sent directly to them
-    @messages = Message.includes(:recipients).where(:message_type_id => [1, 2], :recipients => {:id => @user_id.id})
+    m = Message.includes(:recipients).where(:message_type_id => [1, 2], :recipients => {:id => user.id})
+    @messages = @messages + m.flatten
 
     # Look up messages sent to their OUs
-    
+    user.groups.each do |group|
+      m = Message.includes(:recipients).where(:message_type_id => [1, 2], :recipients => {:id => group.id})
+      @messages = @messages + m.flatten
+    end
     
     # Look up messages sent to their groups
-    
+    user.ous.each do |ou|
+      m = Message.includes(:recipients).where(:message_type_id => [1, 2], :recipients => {:id => ou.id})
+      @messages = @messages + m.flatten
+    end
 
     respond_to do |format|
       format.xml
